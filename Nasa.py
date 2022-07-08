69d69df1f2d2de5209552d02329736a72886e167
gliuyhtgk <108678687+gliuyhtgk@users.noreply.github.com>
2022-07-07 20:42:54 -0400

    Update Nasa.py

--- Nasa.py
+++ Nasa.py
@@ -23,7 +23,7 @@ def NasaNews(Date):
 
     speak('sir i am connecting to cortana cloud....')
 
-    Url = "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0" + str(Api_key)
+    Url = "https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY" + str(Api_key)
 
     Params = {'date':str(Date)}
     
